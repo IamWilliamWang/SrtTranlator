210430ed3be371b8f2e58bb8242cfa16d340e35b
William Wang <iamjerichoholic@hotmail.com>
2019-03-19 00:39:06 +0800

    2018-9-6第一次提交

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SrtTranlatorGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.textBoxFilename = new System.Windows.Forms.TextBox();
+            this.button选择文件 = new System.Windows.Forms.Button();
+            this.buttonRight = new System.Windows.Forms.Button();
+            this.buttonLeft = new System.Windows.Forms.Button();
+            this.textBoxLeft = new System.Windows.Forms.TextBox();
+            this.textBoxRight = new System.Windows.Forms.TextBox();
+            this.button粘贴 = new System.Windows.Forms.Button();
+            this.button复制右边 = new System.Windows.Forms.Button();
+            this.button存储左边 = new System.Windows.Forms.Button();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.Filter = "字幕文件|*.srt|所有文件|*.*";
+            this.openFileDialog.Title = "选择要翻译的字幕文件";
+            // 
+            // textBoxFilename
+            // 
+            this.textBoxFilename.AllowDrop = true;
+            this.textBoxFilename.Location = new System.Drawing.Point(12, 12);
+            this.textBoxFilename.Name = "textBoxFilename";
+            this.textBoxFilename.Size = new System.Drawing.Size(161, 21);
+            this.textBoxFilename.TabIndex = 0;
+            this.textBoxFilename.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxFilename_DragDrop);
+            this.textBoxFilename.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxFilename_DragEnter);
+            // 
+            // button选择文件
+            // 
+            this.button选择文件.Location = new System.Drawing.Point(179, 10);
+            this.button选择文件.Name = "button选择文件";
+            this.button选择文件.Size = new System.Drawing.Size(62, 23);
+            this.button选择文件.TabIndex = 1;
+            this.button选择文件.Text = "选择文件";
+            this.button选择文件.UseVisualStyleBackColor = true;
+            this.button选择文件.Click += new System.EventHandler(this.button选择文件_Click);
+            // 
+            // buttonRight
+            // 
+            this.buttonRight.Location = new System.Drawing.Point(247, 10);
+            this.buttonRight.Name = "buttonRight";
+            this.buttonRight.Size = new System.Drawing.Size(62, 23);
+            this.buttonRight.TabIndex = 2;
+            this.buttonRight.Text = "→";
+            this.buttonRight.UseVisualStyleBackColor = true;
+            this.buttonRight.Click += new System.EventHandler(this.buttonRight_Click);
+            // 
+            // buttonLeft
+            // 
+            this.buttonLeft.Location = new System.Drawing.Point(451, 10);
+            this.buttonLeft.Name = "buttonLeft";
+            this.buttonLeft.Size = new System.Drawing.Size(62, 23);
+            this.buttonLeft.TabIndex = 3;
+            this.buttonLeft.Text = "←";
+            this.buttonLeft.UseVisualStyleBackColor = true;
+            this.buttonLeft.Click += new System.EventHandler(this.buttonLeft_Click);
+            // 
+            // textBoxLeft
+            // 
+            this.textBoxLeft.Font = new System.Drawing.Font("宋体", 7F);
+            this.textBoxLeft.Location = new System.Drawing.Point(12, 40);
+            this.textBoxLeft.Multiline = true;
+            this.textBoxLeft.Name = "textBoxLeft";
+            this.textBoxLeft.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLeft.Size = new System.Drawing.Size(286, 312);
+            this.textBoxLeft.TabIndex = 4;
+            // 
+            // textBoxRight
+            // 
+            this.textBoxRight.Font = new System.Drawing.Font("宋体", 7F);
+            this.textBoxRight.Location = new System.Drawing.Point(304, 40);
+            this.textBoxRight.Multiline = true;
+            this.textBoxRight.Name = "textBoxRight";
+            this.textBoxRight.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxRight.Size = new System.Drawing.Size(271, 312);
+            this.textBoxRight.TabIndex = 5;
+            // 
+            // button粘贴
+            // 
+            this.button粘贴.Location = new System.Drawing.Point(383, 10);
+            this.button粘贴.Name = "button粘贴";
+            this.button粘贴.Size = new System.Drawing.Size(62, 23);
+            this.button粘贴.TabIndex = 6;
+            this.button粘贴.Text = "粘贴";
+            this.button粘贴.UseVisualStyleBackColor = true;
+            this.button粘贴.Click += new System.EventHandler(this.button粘贴_Click);
+            // 
+            // button复制右边
+            // 
+            this.button复制右边.Location = new System.Drawing.Point(315, 10);
+            this.button复制右边.Name = "button复制右边";
+            this.button复制右边.Size = new System.Drawing.Size(62, 23);
+            this.button复制右边.TabIndex = 7;
+            this.button复制右边.Text = "复制右边";
+            this.button复制右边.UseVisualStyleBackColor = true;
+            this.button复制右边.Click += new System.EventHandler(this.button复制右边_Click);
+            // 
+            // button存储左边
+            // 
+            this.button存储左边.Location = new System.Drawing.Point(519, 10);
+            this.button存储左边.Name = "button存储左边";
+            this.button存储左边.Size = new System.Drawing.Size(62, 23);
+            this.button存储左边.TabIndex = 8;
+            this.button存储左边.Text = "存储左边";
+            this.button存储左边.UseVisualStyleBackColor = true;
+            this.button存储左边.Click += new System.EventHandler(this.button存储左边_Click);
+            // 
+            // saveFileDialog
+            // 
+            this.saveFileDialog.Filter = "字幕文件|*.srt|所有文件|*.*";
+            this.saveFileDialog.Title = "选择要保存的字幕文件路径";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 364);
+            this.Controls.Add(this.button存储左边);
+            this.Controls.Add(this.button复制右边);
+            this.Controls.Add(this.button粘贴);
+            this.Controls.Add(this.textBoxRight);
+            this.Controls.Add(this.textBoxLeft);
+            this.Controls.Add(this.buttonLeft);
+            this.Controls.Add(this.buttonRight);
+            this.Controls.Add(this.button选择文件);
+            this.Controls.Add(this.textBoxFilename);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "srt文件转换器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.TextBox textBoxFilename;
+        private System.Windows.Forms.Button button选择文件;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.TextBox textBoxLeft;
+        private System.Windows.Forms.TextBox textBoxRight;
+        private System.Windows.Forms.Button button粘贴;
+        private System.Windows.Forms.Button button复制右边;
+        private System.Windows.Forms.Button button存储左边;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SrtTranlatorGUI
+{
+    public partial class Form1 : Form
+    {
+        String[] originalLines = null;
+        String originalTexts = null;
+        String[] translatedLines = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ReadAllText(string filename)
+        {
+            StringBuilder fullText = new StringBuilder();
+            using (FileStream file = new FileStream(filename, FileMode.Open))
+            {
+                StreamReader reader = new StreamReader(file);
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    fullText.AppendLine(line);
+                }
+            }
+            return fullText.ToString().TrimEnd(new char[] { '\n', '\r' });
+        }
+
+        private void button选择文件_Click(object sender, EventArgs e)
+        {
+            openFileDialog.ShowDialog();
+            this.textBoxFilename.Text = openFileDialog.FileName;
+            originalTexts = ReadAllText(openFileDialog.FileName);
+            originalLines = originalTexts.Replace("\r","").Split('\n');
+            textBoxLeft.Lines = originalLines;
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            StringBuilder textbox3Str = new StringBuilder();
+            for (int lineIndex = 0; lineIndex < originalLines.Count(); lineIndex++) 
+            {
+                if (originalLines[lineIndex].Contains(" --> "))
+                    textbox3Str.AppendLine(originalLines[++lineIndex]);
+            }
+            textBoxRight.Text = textbox3Str.ToString();
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            translatedLines = this.textBoxRight.Text.Replace("\r","").Split('\n');
+            int i = 0, j = 0;
+            for(;i<originalLines.Count();i++)
+            {
+                string line = originalLines[i];
+                if (line.Contains(" --> "))
+                {
+                    i++;
+                    if (IsEmptyString(originalLines[i]))
+                        continue;
+                    try
+                    {
+                        originalLines[i] = translatedLines[j++];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (j < translatedLines.Length || i < originalLines.Count())
+            {
+                this.originalTexts = this.textBoxLeft.Text;
+                this.originalLines = originalTexts.Replace("\r", "").Split('\n');
+                MessageBox.Show("检测到右方行数与左边对应行数不一致，请重新检查译文的正确性！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            printNewTexts();
+        }
+
+        private bool IsEmptyString(string str)
+        {
+            StringBuilder sb = new StringBuilder(str);
+            return 
+                sb.Replace(" ","").Replace("\n","").Replace("\r","").Replace("\t","")
+                  .ToString() == "";
+        }
+
+        private void printNewTexts()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach(string line in originalLines)
+            {
+                sb.AppendLine(line);
+            }
+            this.textBoxLeft.Text = sb.ToString();
+        }
+
+        private void button粘贴_Click(object sender, EventArgs e)
+        {
+            this.textBoxRight.Text = Clipboard.GetText();
+        }
+
+        private void button复制右边_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.textBoxRight.Text);
+        }
+
+        private void button存储左边_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.ShowDialog();
+            File.WriteAllLines(saveFileDialog.FileName, this.textBoxLeft.Lines);
+        }
+
+        private void textBoxFilename_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBoxFilename_DragDrop(object sender, DragEventArgs e)
+        {
+            this.textBoxFilename.Text = ( (string[]) e.Data.GetData(DataFormats.FileDrop) )[0];
+            originalTexts = ReadAllText(this.textBoxFilename.Text);
+            originalLines = originalTexts.Replace("\r","").Split('\n');
+            textBoxLeft.Lines = originalLines;
+        }
+    }
+}
